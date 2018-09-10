84ef5e38301896826e914f418282794b5cbd4ebf
Otavio Santana <otaviopolianasantana@gmail.com>
2018-09-10 17:35:22 -0300

    fixes column

--- archetype-se/se-column/src/main/resources/archetype-resources/src/main/java/App.java
+++ archetype-se/se-column/src/main/resources/archetype-resources/src/main/java/App.java
@@ -14,7 +14,7 @@
  */
 package $package;
 
-import org.jnosql.artemis.document.DocumentTemplate;
+import org.jnosql.artemis.column.ColumnTemplate;
 
 import javax.enterprise.inject.se.SeContainer;
 import javax.enterprise.inject.se.SeContainerInitializer;
@@ -23,7 +23,7 @@ public class App {
 
     public static void main(String[] args) {
         try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
-            DocumentTemplate template = container.select(DocumentTemplate.class).get();
+            ColumnTemplate template = container.select(ColumnTemplate.class).get();
         }
     }
 }
